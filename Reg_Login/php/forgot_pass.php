0a1edcaa0f74cd9fa75a4071d6ceb6a4cde20da0
Hasraj3344 <145811406+Hasraj3344@users.noreply.github.com>
2024-03-18 18:44:08 -0500

    Update forgot_pass.php

--- Reg_Login/php/forgot_pass.php
+++ Reg_Login/php/forgot_pass.php
@@ -5,16 +5,54 @@ $dbusername = "root";
 $dbpassword = "";
 $dbname = "healthConsultancy";
 
+require '/Applications/XAMPP/xamppfiles/lib/php/PHPMailer/src/Exception.php';
+require '/Applications/XAMPP/xamppfiles/lib/php/PHPMailer/src/PHPMailer.php';
+require '/Applications/XAMPP/xamppfiles/lib/php/PHPMailer/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
 
-if (isset($_POST['submit'])) {
+
+
+
+
+    if (isset($_POST['submit'])) {
     // Retrieve the email address
     $email = $_POST['email'];
     
     $select = " SELECT * FROM users WHERE Email = '$email' ";
     $result = mysqli_query($conn, $select);
 
+    $row = mysqli_fetch_array($result);
+    $name = $row['Name'];
+
     if (mysqli_num_rows($result) > 0) {
+        $mail = new PHPMailer();
+          $mail->isSMTP();
+          $mail->Host = 'sandbox.smtp.mailtrap.io';
+          $mail->SMTPAuth = true;
+          $mail->Port = 2525;
+          $mail->Username = '34fca8e46805cf';
+          $mail->Password = 'aea572039bfb25';
+
+          $mail->IsHTML(true);
+          $mail->AddAddress($email,$name);
+          $mail->SetFrom("healthConsult@gmail.com", "Health Consultancy");
+          $mail->AddReplyTo("healthConsult@gmail.com", "Health Consultancy");
+          $mail->Subject = "Reg:Password change";
+
+          $content= "<b>Dear ".$name.",<br><br>
+
+               I hope this message finds you well.<br><br>
+               click this link to<a href='https://6324dev.000webhostapp.com/php/reset_pass.php'> reset your Password.</a>
+               Thank You."
+        $mail->MsgHTML($content); 
+    if(!$mail->Send()) {
+          echo "Error while sending Email.";
+          var_dump($mail);
+     } else {
         echo "<script>alert('Password reset link sent');</script>";
     echo "<script>window.location = '../html/login.html';</script>";
         
@@ -23,6 +61,7 @@ if (isset($_POST['submit'])) {
         $error[] = 'User is not registered';
     }
 }
+
 ?>
 
 <!DOCTYPE html>
