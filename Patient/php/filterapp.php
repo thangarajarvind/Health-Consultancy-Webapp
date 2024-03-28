6e63d04d7dedb58806f7f1e9c5db3574abbaaf0a
Hasraj3344 <145811406+Hasraj3344@users.noreply.github.com>
2024-03-27 23:02:04 -0500

    Update filterapp.php

--- Patient/php/filterapp.php
+++ Patient/php/filterapp.php
@@ -15,7 +15,7 @@
      
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-     <link rel="stylesheet" href="filterapp.css" type="text/css">
+     <link rel="stylesheet" href="../css/filterapp.css" type="text/css">
      
      <style>
           body {
